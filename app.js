322e7a43419c67ce7265c2db87b41d5a77de56c2
RomanAlecTrevino <trevinoalec@gmail.com>
2020-08-02 18:50:23 -0400

    Set up HTML and CSS

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+// Get listeners
+const inputScreen = document.querySelector('.input-screen');
+
+// Track user input
+// Track operator
+    // Run function for that operator
+    // Get second user input
+    // function should return answer
